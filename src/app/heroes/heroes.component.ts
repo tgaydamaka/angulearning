f50084b87d4a00cb9cb8c7b84170a858cf474565
Timofey Gaydamaka <timofey.gaydamaka@eltex.loc>
2018-06-04 17:59:26 +0700

    get heroes asynchronous

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,16 +13,20 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Пётр I'
   };
-  heroes = HEROES;
+  heroes = [];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
